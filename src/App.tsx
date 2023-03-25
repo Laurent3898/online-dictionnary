ffb1a4b0fb5c571e59e77ea5ee6de0106cbd1be6
Laurent-TOA <124306620+Laurent-TOA@users.noreply.github.com>
2023-03-25 11:11:26 +0300

    online dictionnary

--- src/App.tsx
+++ src/App.tsx
@@ -30,16 +30,19 @@ export default function App(): JSX.Element {
     setWord(searchWord);
   }
 
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
   return (
     <QueryClientProvider client={queryClient}>
       <main
         className={`flex flex-col h-screen font-sans subpixel-antialiased bg-gray-100 dark:bg-gray-900 dark:text-gray-50 text-gray-900 ${
           theme === 'dark' ? 'dark' : ''
-        } dotted-background`}
+        } bg-customized`}
       >
         <nav className="flex flex-wrap items-center justify-center md:justify-between py-6 px-6 md:px-12 bg-inherit dark:bg-gray-900 dark:text-gray-50 text-gray-900 ">
           <div className="flex flex-row items-end mr-6 justify-end ">
-            <h1 className="text-xl pb-1 font-bold first-letter:uppercase">
+            <h1 className="text-xl text-slate-900 dark:text-gray-50 pb-1 font-bold first-letter:uppercase">
               Dictionary
             </h1>
           </div>
@@ -47,9 +50,9 @@ export default function App(): JSX.Element {
             onSubmit={handleSubmit}
             className="flex flex-wrap justify-between"
           >
-            <div className="rounded-lg bg-gradient-to-r from-orange-600 via-red-500 to-cyan-500 p-0.5">
+            <div className="rounded-lg bg-gradient-to-r from-orange-600 via-red-500 to-cyan-500 p-0.5  ml-2">
               <input
-                className="w-full max-w-3xl sm:max-w-2xl rounded-md py-2 px-4  focus:outline-none focus:shadow-outline dark:bg-gray-800 bg-gray-50 appearance-none leading-normaL"
+                className="w-full max-w-3xl sm:max-w-2xl rounded-md py-2 px-4  focus:outline-none focus:shadow-outline dark:bg-gray-800 bg-gray-50 appearance-none leading-normaL input-text"
                 type="text"
                 value={searchWord}
                 placeholder="Type any word..."
@@ -69,6 +72,19 @@ export default function App(): JSX.Element {
         </nav>
         <Words word={word} />
         <ReactQueryDevtools />
+        <footer
+          className={` bg-gray-100 dark:bg-gray-900 dark:text-gray-50 text-gray-900 bg-customized`}
+        >
+          <div style={{ textAlign: 'center', padding: '1rem' }}>
+            <p className="text-neutral-700 dark:text-slate-50">
+              copyright &copy;{' '}
+              <b>
+                <span className="variantbluecolor">{currentYear}</span> Laurent
+                R.
+              </b>
+            </p>
+          </div>
+        </footer>
       </main>
     </QueryClientProvider>
   );
